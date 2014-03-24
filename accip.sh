5c04172b00d9c3b9fde7c1e6e798f8bf04d41438
Hisashi Sugiyama <hisashi.sugiyama@gmail.com>
2014-03-24 19:02:34 +0900

    アタックしてきた組織を表示する

--- accip.sh
+++ accip.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sed -n -e '/Failed password/s/.* sshd.*Failed password for.*from \([0-9.]*\) port .*/\1/p' /var/log/secure | sort | uniq | xargs -n1
+sed -n -e '/Failed password/s/.* sshd.*Failed password for.*from \([0-9.]*\) port .*/\1/p' /var/log/secure | sort | uniq | xargs -n1 | whois | grep "Organization"
